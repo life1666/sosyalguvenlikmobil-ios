8d2126d681587cbb26721585579abfe0c8ca8a39
life1666 <sosyalguvenlikcepte@gmail.com>
2025-10-03 23:27:05 +0300

    Remove manual FirebaseApp.configure() to avoid double init

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import FirebaseCore   // ✅ FirebaseCore'u ekle
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +7,6 @@ import FirebaseCore   // ✅ FirebaseCore'u ekle
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()   // ✅ Firebase'i başlat
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
